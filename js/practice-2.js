0a5d69b865246c04fa85792ecdc8748c340454a1
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-04-25 13:40:17 +0300

    Update

---
+++ js/practice-2.js
@@ -0,0 +1,40 @@
+// Task #1
+// function checkAge(age) {
+//   if (age>=18) {
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+
+// Task #2
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+  
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }
+//   return "Access denied, wrong password!";
+// }
+// console.log(checkPassword('mangohackzor'));
+// console.log(checkPassword('polyhax'));
+// console.log(checkPassword('jqueryismyjam'));
+
+// Task #3
+// function checkStorage(available, ordered) {
+//     if (ordered === 0) {
+//     return "Your order is empty!";
+//   } if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   } return "The order is accepted, our manager will contact you";
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
